7f9ceda39893f0e8bf108d076b3a4cb7bec39787
sfo <sfragua83@gmail.com>
2017-12-08 04:13:53 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  bgcolor: string = 'white';
+  textreco: string = '';
 
+  constructor(public navCtrl: NavController, private speechRecognition: SpeechRecognition) {
+  }
+
+  ngOnInit(){
+
+    this.speechRecognition.hasPermission()
+    .then((hasPermission: boolean) => {
+      if(!hasPermission){
+        this.speechRecognition.requestPermission()
+        .then(
+          () => console.log('Granted'),
+          () => console.log('Denied')
+        )
+      }
+    });
+  }
+
+
+  start(){
+    
+    let options = {
+      language: "es-ES"
+    }
+
+    // Get the list of supported languages
+    this.speechRecognition.getSupportedLanguages()
+    .then(
+      (languages: Array<string>) => console.log(languages),
+      (error) => console.log(error)
+    )
+
+    this.speechRecognition.startListening(options).subscribe(
+      (matches: Array<string>)=> {
+        this.bgcolor = matches[0];
+        this.textreco = matches[0];
+      },
+      (onerror) => console.log('error:', onerror)
+    )
   }
 
 }
